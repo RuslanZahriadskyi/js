e2f20a4c4eed8c8cb8ede0cd2bc56dc6d63a4551
RuslanZahriadskyi <RuslanZahriadskyi@users.noreply.github.com>
2020-09-27 11:41:10 +0200

    task5

--- js/homework1/homework1/task5.js
+++ js/homework1/homework1/task5.js
@@ -1,4 +1,4 @@
-const countryName = "КитаЙ";
+const countryName = "ЧиЛи";
 
 const CANCELED_BY_USER = "Отменено пользователем!";
 const NO_DELIVERY = "В выбранную страну доставка недоступна.";
@@ -12,14 +12,26 @@ let country;
 if (countryName === null) {
   message = CANCELED_BY_USER;
 } else {
-  country = countryName[0] + countryName.slice(1, 9); // Write code on this line
-  switch (
-    country
+  country = countryName[0].toUpperCase() + countryName.slice(1).toLowerCase(); // Write code on this line
+  switch (country) {
     // Write code under this line
-  ) {
+    case CHINA:
+      price = 100;
+      break;
+    case AUSTRALIA:
+      price = 170;
+      break;
+    case INDIA:
+      price = 80;
+      break;
+    case JAMAICA:
+      price = 120;
+      break;
+    default:
+      message = NO_DELIVERY;
   }
 }
-if (country) {
+if (price !== 0) {
   // Write code on this line
   message = `Доставка в ${country} будет стоить ${price} кредитов`;
 }
