b320b6aeeb1aa4402df20a86424dbd4356033a08
RuslanZahriadskyi <67427154+RuslanZahriadskyi@users.noreply.github.com>
2020-10-25 02:28:56 +0200

    hm6 task4 complete task5 start

--- js/hm6task3.js
+++ js/hm6task3.js
@@ -85,9 +85,20 @@ const users = [
   },
 ];
 
-const getUsersWithGender = (array, gender) =>
-  array.filter(function ({ name }) {
-    // return name;
-  });
+const getUsersWithGender = (array, genderr) =>
+  array
+    .filter(function ({ gender }) {
+      return gender === genderr;
+    })
+    .map(function ({ name }) {
+      return name;
+    });
 
 console.log(getUsersWithGender(users, 'male'));
+
+// Получи массив имен пользователей (значение свойства name) по полу (значение свойства gender).
+
+// Используй деструктурирующее присваивание для параметра функции ({name}) без пробелов и переносов на новую строку.
+
+// Используй только перебирающие методы массива которые не изменяют(не мутируют) исходный массив.
+//     Т.е.нельзя использовать for, splice, push и т.п.мутирующие методы.
